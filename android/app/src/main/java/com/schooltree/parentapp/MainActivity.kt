aa3dca86a40b336960b4c7ee095771d429c61811
ManiBee237 <jpmanibharathi@gmail.com>
2026-02-15 11:39:32 +0530

    qwerty

--- android/app/src/main/java/com/schooltree/parentapp/MainActivity.kt
+++ android/app/src/main/java/com/schooltree/parentapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.schooltree.parentapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Switch from SplashTheme to AppTheme before React Native renders
+    setTheme(R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
